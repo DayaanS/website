0d424a9dc9deb7e44af4a9a4941b370b1b89dc74
Dayaana <daiaanasemyonova@yandex.ru>
2026-04-21 01:18:56 +0300

    data

--- source/index.js
+++ source/index.js
@@ -35,7 +35,6 @@ function carousel(data) {
 
 }
 
-
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -46,22 +45,6 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
   document.getElementById("cardDefault").display = none;
 }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("card");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// } 
 
 function showSlides() {
   let i;
